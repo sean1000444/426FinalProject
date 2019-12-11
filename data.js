455e2baa2062a8dc675bf67b0a73412a96f05402
Sean McCaffery <sean.mccaffery9@gmail.com>
2019-12-11 10:26:40 -0500

    db connections and movie autocomplete implemented

---
+++ data.js
@@ -0,0 +1,300 @@
+const movieData =[{
+    label: "The Shawshank Redemption", value: "The Shawshank Redemption"
+},
+{
+    label: "The Godfather", value: "The Godfather"
+},
+{
+    label: "The Godfather, Part II", value: "The Godfather, Part II"
+},
+{
+    label: "The Dark Knight", value: "The Dark Knight"
+},
+{
+    label: "12 Angry Men", value: "12 Angry Men"
+},
+{
+    label: "Schindler's List", value: "Schindler's List"
+},
+{
+    label: "Pulp Fiction", value: "Pulp Fiction"
+},
+{
+    label: "The Lord of the Rings: The Return of the King", value: "The Lord of the Rings: The Return of the King"
+},
+{
+    label: "The Good, the Bad, and the Ugly", value: "The Good, the Bad, and the Ugly"
+},
+{
+    label: "Fight Club", value: "Fight Club"
+},
+{
+    label: "The Lord of the Rings: The Fellowship of the Ring", value: "The Lord of the Rings: The Fellowship of the Ring"
+},
+{
+    label: "Forrest Gump", value: "Forrest Gump"
+},
+{
+    label: "Star Wars: Episode V: The Empire Strikes Back", value: "Star Wars: Episode V: The Empire Strikes Back"
+},
+{
+    label: "Inception", value: "Inception"
+},
+{
+    label: "The Lord of the Rings: The Two Towers", value: "The Lord of the Rings: The Two Towers"
+},
+{
+    label: "One Flew Over The Cuckoo's Nest", value: "One Flew Over The Cuckoo's Nest"
+},
+{
+    label: "GoodFellas", value: "GoodFellas"
+},
+{
+    label: "The Matrix", value: "The Matrix"
+},
+{
+    label: "The Seven Samurai", value: "The Seven Samurai"
+},
+{
+    label: "Star Wars", value: "Star Wars"
+},
+{
+    label: "City of God", value: "City of God"
+},
+{
+    label: "Se7en", value: "Se7en"
+},
+{
+    label: "The Silence of the Lambs", value: "The Silence of the Lambs"
+},
+{
+    label: "It's A Wonderful Life", value: "It's A Wonderful Life"
+},
+{
+    label: "Life is Beautiful", value: "Life is Beautiful"
+},
+{
+    label: "The Usual Suspects", value: "The Usual Suspects"
+},
+{
+    label: "Leon, aka The Professional", value: "Leon, aka The Professional"
+},
+{
+    label: "Saving Private Ryan", value: "Saving Private Ryan"
+},
+{
+    label: "Spirited Away", value: "Spirited Away"
+},
+{
+    label: "Coco", value: "Coco"
+},
+{
+    label: "American History X", value: "American History X"
+},
+{
+    label: "Interstellar", value: "Interstellar"
+},
+{
+    label: "Once Upon a Time in the West", value: "Once Upon a Time in the West"
+},
+{
+    label: "The Green Mile", value: "The Green Mile"
+},
+{
+    label: "Psycho", value: "Psycho"
+},
+{
+    label: "Casablanca", value: "Casablanca"
+},
+{
+    label: "City Lights", value: "City Lights"
+},
+{
+    label: "Intouchables", value: "Intouchables"
+},
+{
+    label: "Modern Times", value: "Modern Times"
+},
+{
+    label: "The Pianist", value: "The Pianist"
+},
+{
+    label: "Raiders of the Lost Ark", value: "Raiders of the Lost Ark"
+},
+{
+    label: "The Departed", value: "The Departed"
+},
+{
+    label: "Rear Window", value: "Rear Window"
+},
+{
+    label: "Terminator 2: Judgment Day", value: "Terminator 2: Judgment Day"
+},
+{
+    label: "Back to the Future", value: "Back to the Future"
+},
+{
+    label: "Whiplash", value: "Whiplash"
+},
+{
+    label: "Gladiator", value: "Gladiator"
+},
+{
+    label: "The Lion King", value: "The Lion King"
+},
+{
+    label: "The Prestige", value: "The Prestige"
+},
+{
+    label: "Memento", value: "Memento"
+},
+{
+    label: "Apocalypse Now", value: "Apocalypse Now"
+},
+{
+    label: "Alien", value: "Alien"
+},
+{
+    label: "The Great Dictator", value: "The Great Dictator"
+},
+{
+    label: "Sunset Boulevard", value: "Sunset Boulevard"
+},
+{
+    label: "Cinema Paradiso", value: "Cinema Paradiso"
+},
+{
+    label: "Dr. Strangelove or: How I Learned To Stop Worrying and Love the Bomb", value: "Dr. Strangelove or: How I Learned To Stop Worrying and Love the Bomb"
+},
+{
+    label: "The Lives of Others", value: "The Lives of Others"
+},
+{
+    label: "Grave of the Fireflies", value: "Grave of the Fireflies"
+},
+{
+    label: "Paths of Glory", value: "Paths of Glory"
+},
+{
+    label: "Django Unchained", value: "Django Unchained"
+},
+{
+    label: "The Shining", value: "The Shining"
+},
+{
+    label: "WALL-E", value: "WALL-E"
+},
+{
+    label: "American Beauty", value: "American Beauty"
+},
+{
+    label: "Princess Mononoke", value: "Princess Mononoke"
+},
+{
+    label: "The Dark Knight Rises", value: "The Dark Knight Rises"
+},
+{
+    label: "Blade Runner 2049", value: "Blade Runner 2049"
+},
+{
+    label: "Oldboy", value: "Oldboy"
+},
+{
+    label: "Witness For the Prosecution", value: "Witness For the Prosecution"
+},
+{
+    label: "Aliens", value: "Aliens"
+},
+{
+    label: "Once Upon a Time in America", value: "Once Upon a Time in America"
+},
+{
+    label: "Das Boot", value: "Das Boot"
+},
+{
+    label: "Dangal", value: "Dangal"
+},
+{
+    label: "Citizen Kane", value: "Citizen Kane"
+},
+{
+    label: "Vertigo", value: "Vertigo"
+},
+{
+    label: "North By Northwest", value: "North By Northwest"
+},
+{
+    label: "Star Wars: Episode VI - Return of the Jedi", value: "Star Wars: Episode VI - Return of the Jedi"
+},
+{
+    label: "Braveheart", value: "Braveheart"
+},
+{
+    label: "Reservoir Dogs", value: "Reservoir Dogs"
+},
+{
+    label: "M", value: "M"
+},
+{
+    label: "Requiem for a Dream", value: "Requiem for a Dream"
+},
+{
+    label: "Your Name", value: "Your Name"
+},
+{
+    label: "Like Stars on Earth", value: "Like Stars on Earth"
+},
+{
+    label: "Amelie", value: "Amelie"
+},
+{
+    label: "A Clockwork Orange", value: "A Clockwork Orange"
+},
+{
+    label: "Lawrence of Arabia", value: "Lawrence of Arabia"
+},
+{
+    label: "Amadeus", value: "Amadeus"
+},
+{
+    label: "Double Indemnity", value: "Double Indemnity"
+},
+{
+    label: "Eternal Sunshine of the Spotless Mind", value: "Eternal Sunshine of the Spotless Mind"
+},
+{
+    label: "Taxi Driver", value: "Taxi Driver"
+},
+{
+    label: "To Kill A Mockingbird", value: "To Kill A Mockingbird"
+},
+{
+    label: "Full Metal Jacket", value: "Full Metal Jacket"
+},
+{
+    label: "Singin' In The Rain", value: "Singin' In The Rain"
+},
+{
+    label: "2001: A Space Odyssey", value: "2001: A Space Odyssey"
+},
+{
+    label: "Toy Story", value: "Toy Story"
+},
+{
+    label: "3 Idiots", value: "3 Idiots"
+},
+{
+    label: "The Sting", value: "The Sting"
+},
+{
+    label: "Toy Story 3", value: "Toy Story 3"
+},
+{
+    label: "Inglourious Basterds", value: "Inglourious Basterds"
+},
+{
+    label: "The Bicycle Thief", value: "The Bicycle Thief"
+},
+{
+    label: "The Kid", value: "The Kid"
+}];
--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// UTILITY FUNCTIONS
-
 // Page Loads
-function goToAccountInfoPage(event){
-    alert(currentUser);
-    $("#root").empty().append(
-        "Account Info Page<br>" +
-        "<button id=\"back-to-home-button\">Back to Home Page</button><br>"
-    );
+jwt = ""
+first = ""
+last = ""
+
+async function getAllAuthors() {
+    return await pubRoot.get();
 }
 
-function goToLoginPage(event){
+async function goToLoginPage(event) {
     $("#root").empty().append(
         "Login Page <br>" +
         "<div class=\"login-container\">" +
-            "<input id=\"username-field\" placeholder=\"Username\" name=\"username\"> <br>" +
-            "<input id=\"password-field\" placeholder=\"Password\" name=\"password\"> <br>" +
-            "<button id=\"submit-login-info\">Submit</button>" +
+        "<input id=\"username-field\" placeholder=\"Username\" name=\"username\"> <br>" +
+        "<input id=\"password-field\" placeholder=\"Password\" name=\"password\"> <br>" +
+        "<button id=\"submit-login-info\">Submit</button>" +
         "</div>" +
-        "<button id=\"create-new-account\">Create New Account</button>"
+        "<button id=\"create-new-account\">Create New Account</button>" +
+        "<p>" + jwt + "</p>"
     );
 }
 
-function goToSearchResultsPage(event){
+function goToSearchResultsPage(event) {
     $("#root").empty().append(
         "Search Results Page (Maybe have this pop up so we don't lose the search) <br>" +
         "<button id=\"back-to-home-button\">Back to Home Page</button><br>" +
@@ -31,77 +30,152 @@ function goToSearchResultsPage(event){
     );
 }
 
-function goToHomePage(event){
+function goToAccountInfoPage() {
+    $("#root").empty().append(
+        "Account Info Page<br>" +
+        "<p>Current User: " + first + " " + last + "<\p>" +
+        "<button id=\"back-to-home-button\">Back to Home Page</button><br>"
+    );
+}
+
+function goToHomePage() {
     $("#root").empty().append(
         "Home Page <br>" +
         "<form>" +
-            "<input type=\"text\" placeholder=\"Search\"> <br>" +
+        "<input id=\"search-bar\" type=\"text\" placeholder=\"Search\"> <br>" +
         "</form>" +
         "<button id=\"search-button\">Submit</button> <br>" +
+        "<form>" +
+        "<input id=nameInput type=\"text\" placeholder=\"Name\">" +
+        "<input id=ratingInput type=\"text\" placeholder=\"Rating\">" +
+        "</form>" +
+        "<button id=\"submit-rating-button\">Submit</button> <br>" +
         "<button id=\"account-info-button\">Account Info</button> <br>" +
         "<button id=\"logout-button\">Logout</button> <br>"
     );
+    var countries = movieData;
+    
+    var input = document.getElementById("search-bar");
+    
+    autocomplete({
+        input: input,
+        fetch: function(text, update) {
+            text = text.toLowerCase();
+            // you can also use AJAX requests instead of preloaded data
+            var suggestions = countries.filter(n => n.label.toLowerCase().startsWith(text))
+            update(suggestions);
+        },
+        onSelect: function(item) {
+            input.value = item.label;
+        }
+    });
 }
 
-function goToCreateNewAccountPage(event){
+function goToCreateNewAccountPage(event) {
     $("#root").empty().append(
-        "New Account Page<br>" + 
-        "<form>" + 
-            "<input type=\"text\" placeholder=\"Name\"><br>" + 
-            "<input type=\"text\" placeholder=\"Email\"><br>" + 
-            "<input type=\"text\" placeholder=\"Password\"><br>" + 
-            "<select name=\"Grade\">" + 
-                "<option value=\"2\">2</option>" + 
-                "<option value=\"3\">3</option>" + 
-                "<option value=\"4\">4</option>" + 
-                "<option value=\"5\">5</option>" + 
-                "<option value=\"6\">6</option>" + 
-                "<option value=\"7\">7</option>" + 
-                "<option value=\"8\">8</option>" + 
-                "<option value=\"9\">9</option>" + 
-            "</select><br>" + 
-            "<select name=\"Subject\">" + 
-                "<option value=\"math\">Math</option>" + 
-                "<option value=\"science\">Science</option>" + 
-            "</select><br>" + 
-        "</form>" + 
-        "<button id=\"submit-new-account-info\" type=\"submit\">Create New Account</button> <br>" + 
-        "<button id=\"back-to-login-page\" type=\"submit\">Go Back to Login Page</button>"
+        "New Account Page<br>" +
+        "<form>" +
+        "<input id=\"first\" type=\"text\" placeholder=\"First Name\"><br>" +
+        "<input id=\"last\" type=\"text\" placeholder=\"Last Name\"><br>" +
+        "<input id=\"email\" type=\"text\" placeholder=\"Email\"><br>" +
+        "<input id=\"pass\" type=\"text\" placeholder=\"Password\"><br>" +
+        "</form>" +
+        "<button id=\"submit-new-account-info\" type=\"button\">Create New Account</button> <br>" +
+        "<button id=\"back-to-login-page\">Go Back to Login Page</button>"
     );
 }
 
-function submitLoginInfo(event){
+async function submitLoginInfo(event) {
     // Get the pieces of info (will have to use submit with the Create New Account, so figure that out, then implement the info grab)
     // Check against DB to see if these are correct
-        // If so
-            // Set new current user
-            currentUser = "Sean";
-            // Transfer to new page
-            goToHomePage();
-        // If not alert and keep on the same page
-            // alert("Incorrect username or password");
-}
-
-function submitNewAccountInfo(event){
-    // Save the information to the DB
-    // Set new current user
+    username = $("#username-field").val();
+    pass = $("#password-field").val();
+    const result = await axios ({
+        method: 'post',
+        url: 'http://localhost:3000/account/login', 
+        data: {
+            'name': username,
+            'pass': pass
+        }
+    })
+    console.log(result)
+    jwt = result["data"]["jwt"]
+    first = result["data"]["data"]["first"]
+    last = result["data"]["data"]["last"]
+    // If so, transfer to new page
     goToHomePage();
 }
 
-function submitSearch(event){
+async function submitNewAccountInfo(event) {
+    let first =  $("#first").val();
+    let last =  $("#last").val();
+    let email =  $("#email").val();
+    let pass =  $("#pass").val();
+    // Save the information to the DB
+    const result = await axios ({
+        method: 'post',
+        url: 'http://localhost:3000/account/create', 
+        data: {
+            'name': email,
+            'pass': pass,
+            'data': {
+                first: first,
+                last: last
+            }
+        }
+    })
+    console.log(result);
+    goToLoginPage();
+}
+
+function submitSearch(event) {
     // Get info and use it to augment the following function
     goToSearchResultsPage();
 }
-function logout(event){
-    currentUser = "";
+
+async function submitRating(event) {
+    // Get the info from the event
+    movieName = ""
+    movieRating = ""
+    // Update user's info
+    const userResult = await axios ({
+        method: 'post',
+        url: 'http://localhost:3000/user/movies/movieName', 
+        data: {
+            rating: movieRating,
+        },
+        headers: { Authorization: `Bearer ${jwt}` }
+    })
+    console.log(userResult);
+    
+    // Update private's info
+    const privateResult = await axios ({
+        method: 'post',
+        url: 'http://localhost:3000/private/movies/movieName/ratings', 
+        data: [movieRating],
+        type: "merge",
+        headers: { Authorization: `Bearer ${jwt}` }
+    })
+    console.log(privateResult);
+}
+
+function submitSearch(event) {
+    // Get info and use it to augment the following function
+    goToSearchResultsPage();
+}
+
+function logout(event) {
+    jwt = "Logged Out";
     goToLoginPage();
 }
 
 
 // ORIGINAL PAGE LOAD
-$(function() {
+$(function () {
     const $root = $('#root');
-    currentUser = "";
+    jwt = "";
+    first = "";
+    last = "";
 
     // Login page
     $root.on("click", "#submit-login-info", submitLoginInfo);
@@ -110,16 +184,17 @@ $(function() {
     //New account page
     $root.on("click", "#submit-new-account-info", submitNewAccountInfo);
     $root.on("click", "#back-to-login-page", goToLoginPage);
-    
+
     // Home page
     $root.on("click", "#search-button", submitSearch);
+    $root.on("click", "#submit-rating-button", submitRating);
     $root.on("click", "#account-info-button", goToAccountInfoPage);
     $root.on("click", "#logout-button", logout);
-    
+
     // Account Info and Search Results pages
     $root.on("click", "#back-to-home-button", goToHomePage);
 
     // Search Results page
     //     Deal with however I'm going to do these results
-    
+
 }) 
